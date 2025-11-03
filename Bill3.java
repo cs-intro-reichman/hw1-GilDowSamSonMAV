b385706676fc38643c0d8df28d10863807baf1bf
gil dow <gil.dow@runi.ac.il>
2025-11-03 15:56:39 +0200

    New

--- Bill3.java
+++ Bill3.java
@@ -8,7 +8,7 @@ public class  Bill3{
 	    String MoneyString = args[3];
 		int moneyInt = Integer.parseInt(MoneyString) ;
 		double MoneyValue = (double) moneyInt;  
-		System.out.println("Dear " + name3 + ", " + name2 + ", and" + name1 + 
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + 
 		
 		": pay " + Math.ceil(MoneyValue/3)+ " Shekels Each"); 
 		
