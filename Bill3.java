5cdea1e14cb96c5aac36e20881d3b5bff936ef2c
gil dow <gil.dow@runi.ac.il>
2025-11-03 16:04:07 +0200

    new fixed file

--- Bill3.java
+++ Bill3.java
@@ -2,15 +2,15 @@
 public class  Bill3{
 	public static void main(String[] args) {
 
-	    String name1 = args[0];
-		String name2 = args[1];
-		String name3 = args[2];
+	    String a = args[0];
+		String b = args[1];
+		String c = args[2];
 	    String MoneyString = args[3];
 		int moneyInt = Integer.parseInt(MoneyString) ;
 		double MoneyValue = (double) moneyInt;  
-		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + 
+		System.out.println("Dear " + c + ", " + b + ", and " + a + 
 		
-		": pay " + Math.ceil(MoneyValue/3)+ " Shekels Each"); 
+		": pay " + Math.ceil(MoneyValue/3)+ " Shekels Each."); 
 		
 
 
